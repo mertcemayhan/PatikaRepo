fdad8d359bcc960afb3debb1413c9070deb43456
mertcemayhan <68503225+mertcemayhan@users.noreply.github.com>
2021-06-20 20:13:45 +0300

    Add files via upload

---
+++ Java101Pratik/LeapYearCalculate.java
@@ -0,0 +1,26 @@
+package Java101Pratik;
+
+import java.util.Scanner;
+
+public class LeapYearCalculate {
+    public static void main(String[] args) {
+        int year;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Yıl Giriniz : ");
+        year = input.nextInt();
+
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                System.out.println(year + " bir artık yıldır !");
+            } else {
+                System.out.println(year + " bir artık yıldır değildir !");
+            }
+        } else if (year % 4 == 0) {
+            System.out.println(year + " bir artık yıldır !");
+        } else {
+            System.out.println(year + " bir artık yıldır değildir !");
+        }
+    }
+}
+
