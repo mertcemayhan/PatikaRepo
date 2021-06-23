7a36339092bfe6372b35028e3f77c7615f134cb7
mertcemayhan <68503225+mertcemayhan@users.noreply.github.com>
2021-06-23 23:17:19 +0300

    Add files via upload

---
+++ Java101Pratik/ExponentialNumberCalculator.java
@@ -0,0 +1,22 @@
+package Java101Pratik;
+
+import java.util.Scanner;
+
+public class ExponentialNumberCalculator {
+    public static void main(String[] args) {
+        int n, k;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Üssü alınacak sayı: ");
+        n = input.nextInt();
+        System.out.print("Üs olacak sayı: ");
+        k = input.nextInt();
+        int total = 1;
+
+        for (int i = 1; i <= k; i++) {
+            total *= n;
+        }
+        System.out.println("Sonuç: " + total);
+
+    }
+}
