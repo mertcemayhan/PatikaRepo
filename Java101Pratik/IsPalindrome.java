569a6211f167ced5bfe2860e98d53b834b747c9f
mertcemayhan <68503225+mertcemayhan@users.noreply.github.com>
2021-07-03 22:23:23 +0300

    Add files via upload

---
+++ Java101Pratik/IsPalindrome.java
@@ -0,0 +1,25 @@
+package Java101Pratik;
+
+import java.util.Scanner;
+
+public class IsPalindrome {
+    static boolean isPalindrome(int number) {
+        int reverseNumber = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Palindrom olup olmadığı kontrol edilecek sayıyı giriniz: ");
+        number = input.nextInt();
+        int temp = number;
+
+        while (temp != 0) {
+            int remainder = temp % 10;
+            reverseNumber = (reverseNumber * 10) + remainder;
+            temp /= 10;
+        }
+
+        return number == reverseNumber;
+
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(101));
+    }
+}
