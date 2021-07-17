426ca1a9c09bfa5f1839ae00a7735d0560ef390f
mertcemayhan <68503225+mertcemayhan@users.noreply.github.com>
2021-07-17 20:09:01 +0300

    Add files via upload

---
+++ Java101Pratik/LetterB.java
@@ -0,0 +1,29 @@
+package Java101Pratik;
+
+public class LetterB {
+    public static void main(String[] args) {
+        String[][] letter = new String[7][4];
+
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if ((i == 0 || i == 3 || i == 6) && j != 3) {
+                    letter[i][j] = " * ";
+                } else if ((j == 0 || j == 3)) {
+                    letter[i][j] = " * ";
+                    letter[0][3] = "   ";
+                    letter[3][3] = "   ";
+                    letter[6][3] = "   ";
+                } else {
+                    letter[i][j] = "   ";
+                }
+            }
+        }
+
+        for (String[] row : letter) {
+            for (String col : row) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
