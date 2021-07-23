dd4db4082431ae8cde9867cef22f30c08abd5ee2
mertcemayhan <68503225+mertcemayhan@users.noreply.github.com>
2021-07-23 13:43:56 +0300

    Add files via upload

---
+++ Java101Pratik/PalindromicWord.java
@@ -0,0 +1,18 @@
+package Java101Pratik;
+
+public class PalindromicWord {
+    static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("abba"));
+    }
+}
