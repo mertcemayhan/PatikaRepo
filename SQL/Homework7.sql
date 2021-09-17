e360814ea93879140dfea86d3d9b71072de448d2
mertcemayhan <68503225+mertcemayhan@users.noreply.github.com>
2021-09-17 23:42:41 +0300

    Add files via upload

---
+++ SQL/Homework7.sql
@@ -0,0 +1,15 @@
+--Q1.
+SELECT rating, COUNT(*) FROM film
+GROUP BY rating;
+--Q2.
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50;
+--Q3.
+SELECT store_id, COUNT(*) FROM customer
+GROUP BY store_id;
+--Q4.
+SELECT country_id, COUNT(*) FROM city
+GROUP BY country_id
+ORDER BY COUNT(*) DESC
+LIMIT 1;
